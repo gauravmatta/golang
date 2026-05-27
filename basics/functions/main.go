7123d7b12252d5a7944df496ea6202c7c203aa1b
gauravmatta <gaurav.matta.tech@gmail.com>
2026-05-27 23:06:39 +0530

    reverted functions main

--- basics/functions/main.go
+++ basics/functions/main.go
@@ -3,11 +3,22 @@ package main
 import "fmt"
 
 func main() {
-
 	x, y := "Gaurav", "Matta"
 	fmt.Println("Before Swap", x, y)
 	x, y = Swap(x, y)
 	fmt.Println("After Swap", x, y)
+	xn, yn := 20, 10
+	fmt.Println(xn, yn)
+	fmt.Println(Add(xn, yn))
+	fmt.Println(Subtract(xn, yn))
+}
+
+func Add(x, y int) int {
+	return x + y
+}
+
+func Subtract(x, y int) int {
+	return x - y
 }
 
 func Swap(x, y string) (string, string) {
