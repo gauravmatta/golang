365de4f8e5ed4c39a0df25cf55658f95195ec489
gauravmatta <gaurav.matta.tech@gmail.com>
2026-05-28 22:06:12 +0530

    created a struct composition

---
+++ basics/struct/employee.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Employee struct {
+	FirstName, LastName string
+	Dob                 time.Time
+	JobTitle, Location  string
+}
+
+func (e Employee) PrintName() {
+	fmt.Printf("\n%s %s\n", e.FirstName, e.LastName)
+}
+
+func (e Employee) PrintDetails() {
+	fmt.Printf("Date of Birth: %s, Job: %s, Location: %s\n", e.Dob.String(), e.JobTitle, e.Location)
+}
+
+type Developer struct {
+	Employee //type embedding for composition
+	Skills   []string
+}
+
+func (d Developer) PrintSkills() {
+	fmt.Printf("Skills: %v\n", d.Skills)
+}
+
+func main() {
+	steve := Developer{ // using literal expression
+		Employee: Employee{
+			FirstName: "Steve",
+			LastName:  "John",
+			Dob:       time.Date(1990, time.February, 17, 0, 0, 0, 0, time.UTC),
+			JobTitle:  "Software Engineer",
+			Location:  "San Fancisco",
+		},
+		Skills: []string{"Go", "Docker", "Kubernetes"},
+	}
+	steve.PrintName()
+	steve.PrintDetails()
+
+	var d Developer
+	d.Employee = Employee{
+		FirstName: "John",
+		LastName:  "Doe",
+	}
+	d.Skills = []string{"Go", "Docker"}
+	d.PrintName()
+	d.PrintDetails()
+}
