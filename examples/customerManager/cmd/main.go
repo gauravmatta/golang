b53efaf9d7eb13c4b109d2858b07ccffcaf8eafb
gaurav matta <gaurav.matta.tech@gmail.com>
2026-05-31 11:00:28 +0530

    Segregated Controller

--- examples/customerManager/cmd/main.go
+++ examples/customerManager/cmd/main.go
@@ -6,53 +6,6 @@ import (
 	"fmt"
 )
 
-type CustomerController struct {
-	repository domain.CustomerRepository
-}
-
-func (cc CustomerController) Create(customer domain.Customer) error {
-	err := cc.repository.Create(customer)
-	if err != nil {
-		return err
-	}
-	fmt.Printf("Customer %s has been created\n", customer)
-	return nil
-}
-
-func (cc CustomerController) Update(id string, customer domain.Customer) error {
-	err := cc.repository.Update(id, customer)
-	if err != nil {
-		return err
-	}
-	fmt.Printf("Customer %s has been updated\n", customer)
-	return nil
-}
-
-func (cc CustomerController) Delete(id string) error {
-	err := cc.repository.Delete(id)
-	if err != nil {
-		return err
-	}
-	fmt.Printf("Customer %s has been deleted\n", id)
-	return nil
-}
-
-func (cc CustomerController) FindById(id string) (domain.Customer, error) {
-	customer, err := cc.repository.FindById(id)
-	if err != nil {
-		return domain.Customer{}, err
-	}
-	return customer, nil
-}
-
-func (cc CustomerController) FindAll() ([]domain.Customer, error) {
-	customers, err := cc.repository.FindAll()
-	if err != nil {
-		return []domain.Customer{}, err
-	}
-	return customers, nil
-}
-
 func main() {
 	controller := CustomerController{
 		repository: memstore.NewCustomerRepository(),
