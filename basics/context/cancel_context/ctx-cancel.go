9b049b1759e2d95b634a8d29892c135ec8aecd0d
gauravmatta <gaurav.matta.tech@gmail.com>
2026-06-25 01:30:59 +0530

    Added cancel call in Time and Cancel type context

---
+++ basics/context/cancel_context/ctx-cancel.go
@@ -0,0 +1,65 @@
+// Example code updated for Go 1.21
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+)
+
+func generateValues(ctx context.Context, counter chan int) {
+	defer close(counter)
+	fmt.Printf("The type of ctx is: %T\n", ctx)
+	n := 1
+	for {
+		time.Sleep(1 * time.Second)
+		select {
+		case <-ctx.Done():
+			fmt.Println("Done channel received")
+			if err := ctx.Err(); err != nil {
+				fmt.Println("ctx-cancel error:", err)
+			}
+			if err := context.Cause(ctx); err != nil {
+				fmt.Println("ctx-cancel cause:", err)
+			}
+			return
+		case counter <- n:
+			n++
+		}
+	}
+}
+
+func contextCauseCall() {
+	causeError := errors.New("goroutine is leaking")
+	ctx, cancel := context.WithCancelCause(context.Background())
+
+	counter := make(chan int)
+	// calling goroutine with ctx-cancel value
+	go generateValues(ctx, counter)
+	for n := range counter {
+		if n == 5 {
+			cancel(causeError)
+		}
+		fmt.Println(n)
+	}
+	fmt.Println("done")
+}
+
+func contextTimeCall() {
+	fmt.Println("Starting Time call :" + time.Now().String())
+	causeError := errors.New("goroutine is leaking")
+	ctx, cancel := context.WithTimeoutCause(context.Background(), 3*time.Second, causeError)
+	defer cancel()
+	counter := make(chan int)
+	go generateValues(ctx, counter)
+	for n := range counter {
+		fmt.Println(n)
+	}
+	fmt.Println("Ending Time call :" + time.Now().String())
+}
+
+func main() {
+	contextCauseCall()
+	contextTimeCall()
+}
