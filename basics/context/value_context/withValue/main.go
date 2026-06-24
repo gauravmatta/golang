1674064853144ed7ede989ac8bf5917c455e9e0f
gauravmatta <gaurav.matta.tech@gmail.com>
2026-06-25 02:26:30 +0530

    Added value call in value context

---
+++ basics/context/value_context/withValue/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+type role struct {
+	user string
+	role string
+}
+
+func doSomething(ctx context.Context) {
+	if ctx.Value("authorization") != "my-auth-token" {
+		fmt.Println("Unauthorized from doSomething")
+		return
+	}
+	fmt.Println("Authorized from doSomething for user:", ctx.Value("user"))
+}
+
+func doAnother(ctx context.Context) {
+	if ctx.Value("authorization") != "my-auth-token" {
+		fmt.Println("Unauthorized from doAnother")
+		return
+	}
+	fmt.Println("Authorized from doAnother for User:", ctx.Value("user"))
+}
+
+func doAdmin(ctx context.Context) {
+	if ctx.Value("authorization") == "my-auth-token" {
+		fmt.Printf("Role:%+v", ctx.Value("role").(role))
+	}
+}
+func main() {
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "authorization", "my-auth-token")
+	ctx = context.WithValue(ctx, "user", "gmatta")
+	doSomething(ctx)
+	doAnother(ctx)
+	ctx = context.WithValue(ctx, "role", role{
+		user: "gmatta",
+		role: "admin",
+	})
+	doAdmin(ctx)
+}
