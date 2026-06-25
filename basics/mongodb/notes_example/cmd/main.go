b9723f364a403d84333ee734f2d65a332a2b4e77
gauravmatta <gaurav.matta.tech@gmail.com>
2026-06-25 21:59:43 +0530

    added mongodb example

---
+++ basics/mongodb/notes_example/cmd/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"mongodb/notes_example/model"
+	"mongodb/notes_example/repository"
+)
+
+func main() {
+	noteRepository, err := repository.NewNoteRepository()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	note := model.Note{
+		Title:       "example note",
+		Description: "example note description",
+	}
+
+	// create, update and get
+	if id, err := noteRepository.Create(note); err == nil {
+		fmt.Println("New record has been created with id:", id)
+		if qNote, err := noteRepository.GetById(id); err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Printf("Query Note:%+v\n", qNote)
+		}
+	} else {
+		fmt.Println("error while creating new Note:", err)
+	}
+	// trying to create with duplicate Note
+	if _, err := noteRepository.Create(note); err != nil {
+		fmt.Println("error while creating new Note:", err)
+	}
+
+	note1 := model.Note{
+		Title:       "mongodb",
+		Description: "mongodb is a NoSQL document database",
+	}
+	id, err := noteRepository.Create(note1)
+	if err == nil {
+		fmt.Println("New record has been created with id:", id)
+	}
+	// update
+	note1.Title = "mongodb database"
+	if err := noteRepository.Update(id, note1); err != nil {
+		fmt.Println("error while updating Note:", err)
+	} else {
+		fmt.Println("Updated Note")
+	}
+	// get all
+	if notes, err := noteRepository.GetAll(); err == nil {
+		fmt.Println("printing notes:")
+		for _, note := range notes {
+			fmt.Printf("%+v\n", note)
+		}
+	}
+
+	if err := noteRepository.Delete(id); err != nil {
+		fmt.Println("error while deleting Note:", err)
+	} else {
+		fmt.Println("Deleted Note")
+	}
+	// get all
+	if notes, err := noteRepository.GetAll(); err == nil {
+		fmt.Println("printing notes after deletion:")
+		for _, note := range notes {
+			fmt.Printf("%+v\n", note)
+		}
+	}
+}
---
+++ basics/mongodb/notes_example/model/model.go
@@ -0,0 +1,26 @@
+package model
+
+import (
+	"errors"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+var ErrNotFound = errors.New("No records found")
+var ErrNoteExists = errors.New("Note title exists")
+
+type Note struct {
+	ID          primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Title       string             `json:"title" bson:"title"`
+	Description string             `json:"description" bson:"description"`
+	CreatedOn   time.Time          `json:"createdon,omitempty" bson:"createdon,omitempty"`
+}
+
+type Repository interface {
+	Create(Note) (string, error)
+	Update(string, Note) error
+	Delete(string) error
+	GetById(string) (Note, error)
+	GetAll() ([]Note, error)
+}
